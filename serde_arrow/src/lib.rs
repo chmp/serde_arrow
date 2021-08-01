5036b7f37faa789e98b6390e5517e3c8671e09e8
Christopher Prohm <mail@cprohm.de>
2021-08-01 15:50:03 +0200

    Add exampe to docs

--- serde_arrow/src/lib.rs
+++ serde_arrow/src/lib.rs
@@ -1,4 +1,36 @@
-//! `serde_arrow` - convert list of structs to arrow record batches
+//! # `serde_arrow` - convert list of structs / maps to arrow record batches
+//!
+//! Usage:
+//!
+//! ```rust
+//! # use serde_arrow::Result;
+//! # use serde::Serialize;
+//! # use std::convert::TryFrom;
+//! #
+//! # fn main() -> Result<()> {
+//! #[derive(Serialize)]
+//! struct Example {
+//!     a: f32,
+//!     b: i32,
+//! }
+//!
+//! let records = vec![
+//!     Example { a: 1.0, b: 1 },
+//!     Example { a: 2.0, b: 2 },
+//!     Example { a: 3.0, b: 3 },
+//! ];
+//!
+//! // try to auto-detect the arrow types, result can be overwritten and customized
+//! let schema = serde_arrow::trace_schema(&records)?;
+//! let schema = arrow::datatypes::Schema::try_from(schema)?;
+//!
+//! let batch = serde_arrow::to_record_batch(&records, schema)?;
+//!
+//! assert_eq!(batch.num_rows(), 3);
+//! assert_eq!(batch.num_columns(), 2);
+//! # Ok(())
+//! # }
+//! ```
 //!
 mod array_builder;
 mod schema;
