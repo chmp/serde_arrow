84ee2ca807bc6c20fa912721e12b08bd7a632566
Christopher Prohm <mail@cprohm.de>
2023-03-21 22:05:02 +0100

    Remove doc string

--- arrow2-to-arrow/src/main.rs
+++ arrow2-to-arrow/src/main.rs
@@ -1,11 +1,3 @@
-//! Example how to convert arrow2 arrays to arrow arrays using the FFI interface
-//!
-//! Relevant docs:
-//!
-//! - https://docs.rs/arrow/latest/arrow/ffi/index.html
-//! - https://docs.rs/arrow2/latest/arrow2/ffi/fn.export_array_to_c.html
-//! - https://docs.rs/arrow2/latest/arrow2/ffi/fn.export_field_to_c.html
-//!
 use arrow::array::ArrayData;
 use arrow2::{
     array::{Array, Int32Array},
