c514b7520ee767c734a3a3a18977857aa6b5b981
Christopher Prohm <mail@cprohm.de>
2024-06-02 18:40:05 +0200

    Add more tests for meta data preservation

--- serde_arrow/src/test_with_arrow/mod.rs
+++ serde_arrow/src/test_with_arrow/mod.rs
@@ -1,7 +1,8 @@
 //! Test with an arrow implementation
 //!
-mod issue_137_schema_like_from_arrow_schema;
+mod issue_35_preserve_metadata;
 mod issue_90_top_level_nulls_in_structs;
+mod issue_137_schema_like_from_arrow_schema;
 mod events;
 mod items_wrapper;
 mod serializer_deserializer;
