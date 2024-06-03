223b4d74b472e85084fec90a6b8063b95d9247dc
Christopher Prohm <mail@cprohm.de>
2024-06-03 17:54:19 +0200

    Refactor form_value impl

--- serde_arrow/src/internal/schema/from_value/mod.rs
+++ serde_arrow/src/internal/schema/from_value/mod.rs
@@ -1,13 +1,12 @@
 use serde::Serialize;
 
-use crate::Result;
+use crate::internal::{error::Result, event::Event, sink, source};
 
 use super::SerdeArrowSchema;
 
 pub fn schema_from_value<T: Serialize + ?Sized>(value: &T) -> Result<SerdeArrowSchema> {
     // simple version of serde-transmute
-    let mut events = Vec::<crate::internal::event::Event>::new();
-    crate::internal::sink::serialize_into_sink(&mut events, value)?;
-    let this: SerdeArrowSchema = crate::internal::source::deserialize_from_source(&events)?;
-    Ok(this)
+    let mut events = Vec::<Event>::new();
+    sink::serialize_into_sink(&mut events, value)?;
+    source::deserialize_from_source(&events)
 }
