ed138a25aa96b6e90e28953bc41ed268ff95a819
Christopher Prohm <mail@cprohm.de>
2024-08-07 22:22:41 +0200

    Test that bools serialized from ints are true for any non-zero value

--- serde_arrow/src/test_with_arrow/impls/bool8.rs
+++ serde_arrow/src/test_with_arrow/impls/bool8.rs
@@ -1,6 +1,11 @@
+use serde::Deserialize;
 use serde_json::json;
 
-use crate::internal::utils::Item;
+use crate::internal::{
+    arrow::{ArrayView, DataType, Field, PrimitiveArrayView},
+    deserializer::Deserializer,
+    utils::{Item, Items},
+};
 
 use super::utils::Test;
 
@@ -22,4 +27,27 @@ fn nullable_bool_as_int8() {
         .serialize(items)
         .deserialize(items)
         .check_nulls(&[&[false, true, false]]);
+}
+
+// from the bool8 specs: false is denoted by the value 0. true can be specified using any non-zero
+// value. Preferably 1.
+#[test]
+fn deserialize_from_not_01_ints() -> crate::internal::error::PanicOnError<()> {
+    let field = Field {
+        name: String::from("item"),
+        data_type: DataType::Int8,
+        nullable: false,
+        metadata: Default::default(),
+    };
+    let view = ArrayView::Int8(PrimitiveArrayView {
+        validity: None,
+        values: &[0, -1, 2, 3, -31, 100, 0, 0],
+    });
+    let deserializer = Deserializer::new(&[field], vec![view])?;
+
+    let Items(actual) = Items::<Vec<bool>>::deserialize(deserializer)?;
+    let expected = vec![false, true, true, true, true, true, false, false];
+    assert_eq!(actual, expected);
+
+    Ok(())
 }
