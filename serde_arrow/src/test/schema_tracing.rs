2198a11070b7241dbc239ff4087942a3cbbf544b
Christopher Prohm <mail@cprohm.de>
2024-08-09 10:57:10 +0200

    Add tests for the known failure cases

---
+++ serde_arrow/src/test/schema_tracing.rs
@@ -0,0 +1,82 @@
+use serde_json::json;
+
+use crate::internal::{
+    error::PanicOnError,
+    schema::{SchemaLike, SerdeArrowSchema, TracingOptions},
+};
+
+// see https://github.com/chmp/serde_arrow/issues/216
+mod json_utf8_null {
+    use super::*;
+
+    macro_rules! test {
+        ($name:ident, $($data:tt)*) => {
+            #[test]
+            fn $name() -> PanicOnError<()> {
+                let expected = SerdeArrowSchema::from_value(&json!([
+                    {"name": "flavor", "data_type": "LargeUtf8", "nullable": true},
+                ]))?;
+
+                let data = json!($($data)*);
+                let actual = SerdeArrowSchema::from_samples(&data, TracingOptions::default())?;
+                assert_eq!(actual, expected);
+                Ok(())
+            }
+        };
+    }
+
+    test!(str_null, [{"flavor": "delicious"}, {"flavor": null}]);
+    test!(null_str, [{"flavor": null}, {"flavor": "delicious"}]);
+}
+
+// A mixture of negative and positive ints is traced as i64
+mod json_i64_null {
+    use super::*;
+
+    macro_rules! test {
+        ($name:ident, $($data:tt)*) => {
+            #[test]
+            fn $name() -> PanicOnError<()> {
+                let expected = SerdeArrowSchema::from_value(&json!([
+                    {"name": "value", "data_type": "I64", "nullable": true},
+                ]))?;
+
+                let data = json!($($data)*);
+                let actual = SerdeArrowSchema::from_samples(&data, TracingOptions::default())?;
+                assert_eq!(actual, expected);
+                Ok(())
+            }
+        };
+    }
+
+    test!(pos_null_neg, [{"value": 42}, {"value": null}, {"value": -13}]);
+    test!(pos_neg_null, [{"value": 42}, {"value": -13}, {"value": null}]);
+    test!(null_neg_pos, [{"value": null}, {"value": -13}, {"value": 42}]);
+    test!(null_pos_neg, [{"value": null}, {"value": 42}, {"value": -13}]);
+    test!(neg_null_pos, [{"value": -13}, {"value": null}, {"value": 42}]);
+    test!(neg_pos, [{"value": -13}, {"value": 42}, {"value": null}]);
+}
+
+// Positive ints are traced as u64
+mod json_u64_null {
+    use super::*;
+
+    macro_rules! test {
+        ($name:ident, $($data:tt)*) => {
+            #[test]
+            fn $name() -> PanicOnError<()> {
+                let expected = SerdeArrowSchema::from_value(&json!([
+                    {"name": "value", "data_type": "U64", "nullable": true},
+                ]))?;
+
+                let data = json!($($data)*);
+                let actual = SerdeArrowSchema::from_samples(&data, TracingOptions::default())?;
+                assert_eq!(actual, expected);
+                Ok(())
+            }
+        };
+    }
+
+    test!(pos_null, [{"value": 42}, {"value": null}]);
+    test!(null_pos, [{"value": null}, {"value": 42}]);
+}
