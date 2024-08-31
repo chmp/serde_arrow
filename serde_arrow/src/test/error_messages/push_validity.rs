e84a8132132793a65b985db085cf259db44b1093
Christopher Prohm <mail@cprohm.de>
2024-08-31 20:09:43 +0200

    Add tests for top-level and struct fields

--- serde_arrow/src/test/error_messages/push_validity.rs
+++ serde_arrow/src/test/error_messages/push_validity.rs
@@ -8,7 +8,7 @@ use crate::internal::{
 };
 
 #[test]
-fn push_validity_issue_202() -> PanicOnError<()> {
+fn int_nested() -> PanicOnError<()> {
     let schema = SerdeArrowSchema::from_value(&json!([
         {
             "name": "nested",
@@ -25,6 +25,60 @@ fn push_validity_issue_202() -> PanicOnError<()> {
 
     let res = array_builder.push(&json!({"nested": {"field": null}}));
     assert_error_contains(&res, "field: \"$.nested.field\"");
+
+    Ok(())
+}
+
+#[test]
+fn int_top_level() -> PanicOnError<()> {
+    let schema = SerdeArrowSchema::from_value(&json!([
+        {"name": "field", "data_type": "U32"},
+    ]))?;
+
+    let mut array_builder = ArrayBuilder::new(schema)?;
+    let res = array_builder.push(&json!({"field": 32}));
+    assert_eq!(res, Ok(()));
+
+    let res = array_builder.push(&json!({"field": null}));
+    assert_error_contains(&res, "field: \"$.field\"");
+
+    Ok(())
+}
+
+#[test]
+fn struct_nested() -> PanicOnError<()> {
+    let schema = SerdeArrowSchema::from_value(&json!([
+        {
+            "name": "nested",
+            "data_type": "Struct",
+            "children": [
+                {"name": "field", "data_type": "Struct", "children": []},
+            ],
+        },
+    ]))?;
+
+    let mut array_builder = ArrayBuilder::new(schema)?;
+    let res = array_builder.push(&json!({"nested": {"field": {}}}));
+    assert_eq!(res, Ok(()));
+
+    let res = array_builder.push(&json!({"nested": {"field": null}}));
+    assert_error_contains(&res, "field: \"$.nested.field\"");
+
+    Ok(())
+}
+
+#[test]
+fn struct_top_level() -> PanicOnError<()> {
+    let schema = SerdeArrowSchema::from_value(&json!([
+        {"name": "field", "data_type": "Struct", "children": []},
+    ]))?;
+
+    let mut array_builder = ArrayBuilder::new(schema)?;
+    let res = array_builder.push(&json!({"field": {}}));
+    assert_eq!(res, Ok(()));
+
+    let res = array_builder.push(&json!({"field": null}));
+    assert_error_contains(&res, "field: \"$.field\"");
 
     Ok(())
 }
