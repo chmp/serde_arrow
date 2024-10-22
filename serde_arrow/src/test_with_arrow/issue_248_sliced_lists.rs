d336b3f4ccacbfdbf5334345081b647f45a305a2
Christopher Prohm <mail@cprohm.de>
2024-10-22 22:09:57 +0200

    Add failing example

---
+++ serde_arrow/src/test_with_arrow/issue_248_sliced_lists.rs
@@ -0,0 +1,43 @@
+use crate::_impl::arrow::datatypes::FieldRef;
+use crate::{
+    self as serde_arrow,
+    schema::{SchemaLike, TracingOptions},
+};
+
+#[test]
+fn strings() {
+    #[derive(serde::Deserialize, serde::Serialize, Debug, PartialEq)]
+    struct Struct {
+        string: String,
+    }
+    let data = (1..=10)
+        .map(|x| Struct {
+            string: x.to_string(),
+        })
+        .collect::<Vec<_>>();
+    let fields = Vec::<FieldRef>::from_type::<Struct>(TracingOptions::default()).unwrap();
+    let batch = serde_arrow::to_record_batch(&fields, &data).unwrap();
+    let batch = batch.slice(5, 5);
+
+    let actual: Vec<Struct> = serde_arrow::from_record_batch(&batch).unwrap();
+    assert_eq!(data[5..10], actual);
+}
+
+#[test]
+fn vec_of_strings() {
+    #[derive(serde::Deserialize, serde::Serialize, Debug, PartialEq)]
+    struct Struct {
+        string: Vec<String>,
+    }
+    let data = (1..=10)
+        .map(|x| Struct {
+            string: vec![x.to_string()],
+        })
+        .collect::<Vec<_>>();
+    let fields = Vec::<FieldRef>::from_type::<Struct>(TracingOptions::default()).unwrap();
+    let batch = serde_arrow::to_record_batch(&fields, &data).unwrap();
+    let batch = batch.slice(5, 5);
+
+    let actual: Vec<Struct> = serde_arrow::from_record_batch(&batch).unwrap();
+    assert_eq!(data[5..10], actual);
+}
