54638c08f17fb6b27f2bb2d7b56d0ca5eb1d494b
Christopher Prohm <mail@cprohm.de>
2024-10-22 22:42:41 +0200

    Fix maps as well

--- serde_arrow/src/test_with_arrow/issue_248_sliced_lists.rs
+++ serde_arrow/src/test_with_arrow/issue_248_sliced_lists.rs
@@ -1,4 +1,7 @@
+use std::collections::HashMap;
+
 use crate::_impl::arrow::datatypes::FieldRef;
+use crate::internal::testing::hash_map;
 use crate::{
     self as serde_arrow,
     schema::{SchemaLike, TracingOptions},
@@ -35,6 +38,27 @@ fn vec_of_strings() {
         })
         .collect::<Vec<_>>();
     let fields = Vec::<FieldRef>::from_type::<Struct>(TracingOptions::default()).unwrap();
+    let batch = serde_arrow::to_record_batch(&fields, &data).unwrap();
+    let batch = batch.slice(5, 5);
+
+    let actual: Vec<Struct> = serde_arrow::from_record_batch(&batch).unwrap();
+    assert_eq!(data[5..10], actual);
+}
+
+#[test]
+fn map_of_strings() {
+    #[derive(serde::Deserialize, serde::Serialize, Debug, PartialEq)]
+    struct Struct {
+        string: HashMap<String, String>,
+    }
+    let data = (1..=10)
+        .map(|x| Struct {
+            string: hash_map!(x.to_string() => x.to_string()),
+        })
+        .collect::<Vec<_>>();
+    let fields =
+        Vec::<FieldRef>::from_type::<Struct>(TracingOptions::default().map_as_struct(false))
+            .unwrap();
     let batch = serde_arrow::to_record_batch(&fields, &data).unwrap();
     let batch = batch.slice(5, 5);
 
