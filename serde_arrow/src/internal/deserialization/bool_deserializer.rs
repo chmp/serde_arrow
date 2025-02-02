eddde5899f2729359075e1d919edca33a7f9c804
Christopher Prohm <mail@cprohm.de>
2025-02-02 14:50:33 +0100

    Fix bool deserializer impl

--- serde_arrow/src/internal/deserialization/bool_deserializer.rs
+++ serde_arrow/src/internal/deserialization/bool_deserializer.rs
@@ -1,10 +1,7 @@
 use marrow::view::BooleanView;
 use serde::de::Visitor;
 
-use crate::internal::{
-    error::{fail, set_default, try_, Context, ContextSupport, Error, Result},
-    utils::Mut,
-};
+use crate::internal::error::{fail, set_default, try_, Context, ContextSupport, Error, Result};
 
 use super::{
     random_access_deserializer::RandomAccessDeserializer, simple_deserializer::SimpleDeserializer,
@@ -14,26 +11,24 @@ use super::{
 pub struct BoolDeserializer<'a> {
     pub path: String,
     pub view: BooleanView<'a>,
-    pub next: usize,
 }
 
 impl<'a> BoolDeserializer<'a> {
     pub fn new(path: String, view: BooleanView<'a>) -> Self {
-        Self {
-            path,
-            view,
-            next: 0,
-        }
+        Self { path, view }
     }
 
     fn get(&self, idx: usize) -> Result<Option<bool>> {
-        if let Some(validty) = &self.view.validity {
-            if !bitset_is_set(validty, idx)? {
+        if idx >= self.view.len {
+            fail!("Out of bounds access");
+        }
+        if let Some(validity) = &self.view.validity {
+            if !bitset_is_set(validity, idx)? {
                 return Ok(None);
             }
         }
 
-        Ok(Some(bitset_is_set(&self.view.values, self.next)?))
+        Ok(Some(bitset_is_set(&self.view.values, idx)?))
     }
 
     fn get_required(&self, idx: usize) -> Result<bool> {
@@ -42,44 +37,6 @@ impl<'a> BoolDeserializer<'a> {
         } else {
             fail!("Required value was not defined");
         }
-    }
-
-    fn next(&mut self) -> Result<Option<bool>> {
-        if self.next >= self.view.len {
-            fail!("Exhausted deserializer");
-        }
-        if let Some(validty) = &self.view.validity {
-            if !bitset_is_set(validty, self.next)? {
-                self.next += 1;
-                return Ok(None);
-            }
-        }
-
-        let val = bitset_is_set(&self.view.values, self.next)?;
-        self.next += 1;
-        Ok(Some(val))
-    }
-
-    fn next_required(&mut self) -> Result<bool> {
-        if let Some(val) = self.next()? {
-            Ok(val)
-        } else {
-            fail!("Missing value");
-        }
-    }
-
-    fn peek_next(&self) -> Result<bool> {
-        if self.next >= self.view.len {
-            fail!("Exhausted deserializer");
-        } else if let Some(validity) = &self.view.validity {
-            bitset_is_set(validity, self.next)
-        } else {
-            Ok(true)
-        }
-    }
-
-    fn consume_next(&mut self) {
-        self.next += 1;
     }
 }
 
@@ -90,67 +47,7 @@ impl Context for BoolDeserializer<'_> {
     }
 }
 
-impl<'de> SimpleDeserializer<'de> for BoolDeserializer<'de> {
-    fn deserialize_any<V: Visitor<'de>>(&mut self, visitor: V) -> Result<V::Value> {
-        try_(|| {
-            if self.peek_next()? {
-                self.deserialize_bool(visitor)
-            } else {
-                self.consume_next();
-                visitor.visit_none::<Error>()
-            }
-        })
-        .ctx(self)
-    }
-
-    fn deserialize_option<V: Visitor<'de>>(&mut self, visitor: V) -> Result<V::Value> {
-        try_(|| {
-            if self.peek_next()? {
-                visitor.visit_some(Mut(self))
-            } else {
-                self.consume_next();
-                visitor.visit_none::<Error>()
-            }
-        })
-        .ctx(self)
-    }
-
-    fn deserialize_bool<V: Visitor<'de>>(&mut self, visitor: V) -> Result<V::Value> {
-        try_(|| visitor.visit_bool::<Error>(self.next_required()?)).ctx(self)
-    }
-
-    fn deserialize_u8<V: Visitor<'de>>(&mut self, visitor: V) -> Result<V::Value> {
-        try_(|| visitor.visit_u8::<Error>(if self.next_required()? { 1 } else { 0 })).ctx(self)
-    }
-
-    fn deserialize_u16<V: Visitor<'de>>(&mut self, visitor: V) -> Result<V::Value> {
-        try_(|| visitor.visit_u16::<Error>(if self.next_required()? { 1 } else { 0 })).ctx(self)
-    }
-
-    fn deserialize_u32<V: Visitor<'de>>(&mut self, visitor: V) -> Result<V::Value> {
-        try_(|| visitor.visit_u32::<Error>(if self.next_required()? { 1 } else { 0 })).ctx(self)
-    }
-
-    fn deserialize_u64<V: Visitor<'de>>(&mut self, visitor: V) -> Result<V::Value> {
-        try_(|| visitor.visit_u64::<Error>(if self.next_required()? { 1 } else { 0 })).ctx(self)
-    }
-
-    fn deserialize_i8<V: Visitor<'de>>(&mut self, visitor: V) -> Result<V::Value> {
-        try_(|| visitor.visit_i8::<Error>(if self.next_required()? { 1 } else { 0 })).ctx(self)
-    }
-
-    fn deserialize_i16<V: Visitor<'de>>(&mut self, visitor: V) -> Result<V::Value> {
-        try_(|| visitor.visit_i16::<Error>(if self.next_required()? { 1 } else { 0 })).ctx(self)
-    }
-
-    fn deserialize_i32<V: Visitor<'de>>(&mut self, visitor: V) -> Result<V::Value> {
-        try_(|| visitor.visit_i32::<Error>(if self.next_required()? { 1 } else { 0 })).ctx(self)
-    }
-
-    fn deserialize_i64<V: Visitor<'de>>(&mut self, visitor: V) -> Result<V::Value> {
-        try_(|| visitor.visit_i64::<Error>(if self.next_required()? { 1 } else { 0 })).ctx(self)
-    }
-}
+impl<'de> SimpleDeserializer<'de> for BoolDeserializer<'de> {}
 
 impl<'de> RandomAccessDeserializer<'de> for BoolDeserializer<'de> {
     fn is_some(&self, idx: usize) -> Result<bool> {
