e1b28988757fbf5c9636ebd09dd468f0396099b2
Raj Sahae <raj@nimble.ai>
2025-03-21 14:35:05 -0700

    wip: possible repro of None issue

--- serde_arrow/src/test_with_arrow/impls/flattened_union.rs
+++ serde_arrow/src/test_with_arrow/impls/flattened_union.rs
@@ -172,8 +172,9 @@ struct MsgOptions {
     loc: Location,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Default)]
 enum Location {
+    #[default]
     Left,
     Right,
 }
@@ -220,10 +221,151 @@ fn test_flattened_union_with_nested_enum() {
     };
 
     let serializer = Serializer::new(builder);
-    nested_enum_data()
+
+    let result = nested_enum_data()
         .serialize(serializer)
         .expect("failed to serialize")
         .into_inner()
         .to_arrow()
         .expect("failed to serialize to arrow");
+
+    println!("arrow: {result:#?}");
+}
+
+#[derive(Serialize, Deserialize)]
+struct OuterSkipped {
+    meta: u32,
+    data: InnerSkipped,
+}
+
+#[derive(Serialize, Deserialize)]
+enum InnerSkipped {
+    One {
+        data: usize,
+    },
+    Two {
+        vector: [i32; 3],
+        #[serde(skip)]
+        location: Location,
+    },
+    Three {
+        // x: Option<InnerData>,
+        // y: Option<InnerData>,
+        y: InnerData,
+    },
+}
+
+#[derive(Serialize, Deserialize, Default)]
+struct InnerData {
+    field1: usize,
+    field2: i32,
+    field3: u32,
+}
+
+fn skipped_schema() -> SerdeArrowSchema {
+    let options = TracingOptions::default()
+        .allow_null_fields(true)
+        .enums_without_data_as_strings(true)
+        .enums_with_named_fields_as_structs(true);
+
+    SerdeArrowSchema::from_type::<OuterSkipped>(options).unwrap()
+}
+
+fn skipped_data() -> Vec<OuterSkipped> {
+    vec![
+        OuterSkipped {
+            meta: 1,
+            data: InnerSkipped::One { data: 1 },
+        },
+        OuterSkipped {
+            meta: 2,
+            data: InnerSkipped::Two {
+                vector: [2, 2, 2],
+                location: Location::Right,
+            },
+        },
+        // OuterSkipped {
+        //     meta: 3,
+        //     data: InnerSkipped::Three {
+        //         /*x: None,*/ y: None,
+        //     },
+        // },
+        OuterSkipped {
+            meta: 4,
+            data: InnerSkipped::Three {
+                // x: None,
+                // y: Some(InnerData {
+                //     field1: 99,
+                //     field2: -99,
+                //     field3: 99,
+                // }),
+                y: InnerData {
+                    field1: 99,
+                    field2: -99,
+                    field3: 99,
+                },
+            },
+        },
+    ]
+}
+
+#[test]
+fn test_flattened_union_with_nested_skipped_fields() {
+    let mut builder = ArrayBuilder::new(skipped_schema()).unwrap();
+
+    // One struct in the array
+    let arrays = builder.build_arrays().unwrap();
+
+    println!("{arrays:#?}");
+
+    assert_eq!(arrays.len(), 2);
+
+    let Array::UInt32(_) = &arrays[0] else {
+        panic!("expected a int array, found {:#?}", &arrays[0]);
+    };
+
+    let Array::Struct(_) = &arrays[1] else {
+        panic!("expected a struct array, found {:#?}", &arrays[1]);
+    };
+
+    let serializer = Serializer::new(builder);
+
+    let result = skipped_data()
+        .serialize(serializer)
+        .expect("failed to serialize")
+        .into_inner()
+        .to_arrow()
+        .expect("failed to serialize to arrow");
+
+    println!("arrow: {result:#?}");
+
+    // TODO: I think this repros the None issue!
+
+    /*
+        -- child 1: "Three::y" (Struct([Field { name: "field1", data_type: UInt64, nullable: false, dict_id: 0, dict_is_ordered: false, metadata: {} }, Field { name: "field2", data_type: Int32, nullable: false, dict_id: 0, dict_is_ordered: false, metadata: {} }, Field { name: "field3", data_type: UInt32, nullable: false, dict_id: 0, dict_is_ordered: false, metadata: {} }]))
+    StructArray
+    [
+    -- child 0: "field1" (UInt64)
+    PrimitiveArray<UInt64>
+    [
+      0,
+      0,
+      0,
+    ]
+    -- child 1: "field2" (Int32)
+    PrimitiveArray<Int32>
+    [
+      0,
+      0,
+      0,
+    ]
+    -- child 2: "field3" (UInt32)
+    PrimitiveArray<UInt32>
+    [
+      0,
+      0,
+      0,
+    ]
+    ]
+    */
 }
