e51ee3d788549f9a6c2f3ded683db5a8c5e563aa
Christopher Prohm <mail@cprohm.de>
2026-06-14 17:19:15 +0200

    Add arrow=59 support

--- marrow/src/impl_arrow/mod.rs
+++ marrow/src/impl_arrow/mod.rs
@@ -2,6 +2,11 @@
 #![cfg_attr(any(), rustfmt::skip)]
 
 // arrow-version:insert: #[cfg(feature = "arrow-{version}")]{\n}mod arrow_{version} {{{\n}    use {{arrow_array_{version} as arrow_array, arrow_buffer_{version} as arrow_buffer, arrow_data_{version} as arrow_data, arrow_schema_{version} as arrow_schema}};{\n}    include!("impl_api_53.rs");{\n}}}
+#[cfg(feature = "arrow-59")]
+mod arrow_59 {
+    use {arrow_array_59 as arrow_array, arrow_buffer_59 as arrow_buffer, arrow_data_59 as arrow_data, arrow_schema_59 as arrow_schema};
+    include!("impl_api_53.rs");
+}
 #[cfg(feature = "arrow-58")]
 mod arrow_58 {
     use {arrow_array_58 as arrow_array, arrow_buffer_58 as arrow_buffer, arrow_data_58 as arrow_data, arrow_schema_58 as arrow_schema};
