735b05aab09dbf67a4e06ee36299f85576037d4d
Christopher Prohm <mail@cprohm.de>
2026-09-23 21:03:09 +0200

    Pin profiling shell to stable nixpkgs

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "Development, benchmarking, and profiling environment for serde_arrow";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
 
   outputs = { nixpkgs, ... }:
     let
